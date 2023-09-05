4aa106992f75216cfa290a68a165f550bb21bbcb
adarshamt <adarshamt@gmail.com>
2023-09-05 15:16:28 +0530

    update

--- controller/productController.js
+++ controller/productController.js
@@ -1,14 +1,16 @@
 const productSchema = require("../model/productSchema")
 
-const cloudinary = require('cloudinary').v2
+ const cloudinary = require('../Cloudinary/cloudinary')
+
+const fs = require('fs')
 
 
-cloudinary.config({ 
-     cloud_name: 'dcy1nhstg', 
-     api_key: '359642551777749', 
-     api_secret: 'ochqSnaOORPVryrlm9q0oFPEXRU' 
-   });
-
+// cloudinary.config({ 
+//      cloud_name: 'dcy1nhstg', 
+//      api_key: '359642551777749', 
+//      api_secret: 'ochqSnaOORPVryrlm9q0oFPEXRU' 
+//    }); 
+//   
 // ********** addd product ***********
 
 const addproduct = async (req,res)=>
@@ -21,42 +23,82 @@ const addproduct = async (req,res)=>
 
                 console.log("req files",req.files)
 
+                 const uploader = async ( path)=> await cloudinary.uploads(path,'images')
+                 
+                 if ( req.method == "POST"){
+
+                    const urls =[]
+                    const files = req.files
+                    
+                    for ( const file of files ){
+                         
+                         const { path } = file
+                         console.log("paths",path)
+                         
+                       const newPath = await uploader(path)
+                         console.log("new path :",newPath)
+                         
+                         urls.push(newPath)
+                         
+                         fs.unlinkSync(path)
+                    }
+
+                    
+                    res.status(200).json({
+                         
+                         message : "image uploaded sussesfull",  
+                    data:urls
+               })
+          }
+               
+                else{
+
+                    res.status(400).json({
+                         err: " image not uploaded"
+                    })
+                }
+
+
+
                   //  const files = req.files.images
                  //  cloudinary.uploader.upload(files.tempFilePath,(err,result)=>{
 
                   //      console.log("results",result)
     
-             cloudinary.uploader.upload(req.files.path,async (err,result)=>{
+
+               //    **************Cloudinar uploading **********
+
+          //    cloudinary.uploader.upload(req.file.path,async (err,result)=>{
 
 
-                    if(err)
-                    {
+                    // if(err)
+                    // {
 
-                       console.log("clodinary error",err)
-                       return res.status(500).json({error:"Error uploading to cloudinary"})
-                    }
+                    //    console.log("clodinary error",err)
+                    //    return res.status(500).json({error:"Error uploading to cloudinary"})
+                    // }
 
                     //   res.status(200).json({url:req.url})
 
-                    console.log("results",result)
+                    // console.log("results",result)
 
-                    const product = new productSchema ({
-                      productName,
-                      discription,
-                      price,
-                      category,
-                      images:  result.secure_url
+                    // const product = new productSchema ({
+                    //   productName,
+                    //   discription,
+                    //   price,
+                    //   category,
+                    //   images:  result.secure_url
                  
-                    })
+                    // })
                  
-                    await product.save()
+                    // await product.save()
                       
-                      res.json({
-                           status:'success',
-                           message:"product created",
-                           data:product
-                      })
-          })
+                    //   res.json({
+                    //        status:'success',
+                    //        message:"product created",
+                    //        data:product
+                    //   })
+          // })
 
      //************adding multiple images ***********/
 
