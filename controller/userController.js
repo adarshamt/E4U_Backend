6901efc2a707dcda47a3317c8beb76b7b2439c9d
adarshamt <adarshamt@gmail.com>
2023-09-17 23:38:55 +0530

    update

--- controller/userController.js
+++ controller/userController.js
@@ -1,112 +1,141 @@
-const user = require('../model/userSchema')
+const user = require("../model/userSchema");
+const product = require("../model/productSchema")
 
+const bcrypt = require("bcrypt");
 
-const bcrypt = require('bcrypt')
+const jwt = require("jsonwebtoken");
+const { link } = require("../routes/storeRoutes");
+const { json } = require("express");
 
-const jwt = require('jsonwebtoken')
-const { link } = require('../routes/storeRoutes')
+const userRegistraion = async (req, res) => {
+  try {
+    //  console.log("req files",req.file)
+    const { name, username, phone, email, password, address, location } =
+      req.body;
 
+    console.log("req name", name);
+    console.log("req files", req.file);
 
+    const hashedPassword = await bcrypt.hash(password, 10);
 
+    const newUser = new user({
+      name: name,
+      username: username,
+      password: hashedPassword,
+      email: email,
+      phoneNumber: phone,
+      address: address,
+      location: location,
+      images: req.file,
+    });
 
-const userRegistraion= async(req,res)=>{
-   
-
-    
-    try{
-        //  console.log("req files",req.file)
-    const {name,username,phone,email,password,address,location}=req.body
-
-    console.log("req name",name) 
-    console.log("req files",req.file)
-
-    const hashedPassword = await bcrypt.hash(password,10)
-    
-    
-    
-    const newUser= new user ({name:name,username:username,password:hashedPassword,email:email,
-                               phoneNumber:phone,address:address,location:location,images:req.file})
-                                
-    //   const {path} = req.file                         
+    //   const {path} = req.file
     //  user.images.push(req.file  )
-    console.log("req file",req.file)
-    
-    
-    await newUser.save()
+    console.log("req file", req.file);
+
+    await newUser.save();
 
     res.status(200).json({
-        status :'success',
-        message:" user registred successfully"
-
-    })
-  
-    }
-
-   catch(error){
-
+      status: "success",
+      message: " user registred successfully",
+    });
+  } catch (error) {
     res.json({
-
-        status:'failed to register',
-        message:error.message
-         })
-  
-     }
-
-} 
+      status: "failed to register",
+      message: error.message,
+    });
+  }
+};
 //****************** user login ***************** */
 
-const login = async(req,res) =>{
-    const {email,password} = req.body
+const login = async (req, res) => {
+  const { email, password } = req.body;
 
-    const checkUser = await user.findOne({email:email})
-    console.log(checkUser)
-    const user_id = checkUser._id
-    console.log(user_id,"----------user id")
+  const checkUser = await user.findOne({ email: email });
+  console.log(checkUser);
+  const user_id = checkUser._id;
+  console.log(user_id, "----------user id");
 
-    if(! checkUser){
-        return res.status(404).json({
-            status:"faliure",
-            message:"invalid email",
-            
-        })
-   
-    }
-   if(! bcrypt.compare(password,checkUser.password)){
-
-      
+  if (!checkUser) {
     return res.status(404).json({
-        status:"faliure",
-        message:"invalid password",
-        
-    })
-   }
- const token = jwt.sign({email:user.email},'adarsh')
-    res.json({
-        
-        status:"success",
-        message:"successfully",
-        email:email,
-        token:token,
-        user_id
-      
-   })
-
-   
-}
+      status: "faliure",
+      message: "invalid email",
+    });
+  }
+  if (!bcrypt.compare(password, checkUser.password)) {
+    return res.status(404).json({
+      status: "faliure",
+      message: "invalid password",
+    });
+  }
+  const token = jwt.sign({ email: user.email }, "adarsh");
+  res.json({
+    status: "success",
+    message: "successfully",
+    email: email,
+    token: token,
+    user_id,
+  });
+};
 
 // ************ add to cart *****************
 
-const addToCart = async(req,res)=>{
-  
-    const { user_id,product_id}=req.body
+const addToCart = async (req, res) => {
+  try {
+    const { user_id, product_id } = req.body;
 
-    console.log(" user id :",user_id," product id :",product_id,"+++++++++++++++++++")
+    const User = await user.findById(user_id);
+
+    
+      if (!User.cart.includes(product_id)) {
+        User.cart.push(product_id);
+        await User.save();
+
+       return res.json({
+          status: 200,
+          message: "add to cart successfull",
+        });
+      } else {
+
+        return res.json({
+
+            status :200,
+            message:" product alredy present in cart"
+        })
+        
+      }
+    
+  } catch (err) {
+    res.json({
+      message: "failed to addcart",
+      error: err,
+    });
+  }
+};
+
+const listCart = async (req,res)=>{
+  
+    const {id} = req.query
+
+    const User = await user.findById(id)
+      
+    let items =[]
+    for ( itm of User.cart){
+     
+    const item = await product.findById(itm)
+
+     items.push(item)
+    }
+
+    res.json({
+          status:200,
+        message : " prduct listed successfully",
+        products:items
+
+    })
+
+    console.log("+++++++++++++++++++ prodcuts array :",items)
 
 }
 
-
-
-
-
-module.exports={userRegistraion,login,addToCart}
-    
+module.exports = { userRegistraion, login, addToCart,listCart };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,6 +12,7 @@ const upload = require('../middlware/fileupload')
 router.post('/user/registraion',upload.single('images'),user.userRegistraion)
 router.post('/user/login',user.login)
 router.post('/user/addtocart',user.addToCart)
+router.get('/user/cart/products',user.listCart)
 
 
 
